34f7f2120d415b0208b2a28c9f9157b2c187a402
Aubrey <aubreyfinkelstein@gmail.com>
2023-03-08 11:38:59 -0700

    configured firestore database for using my own personal firebase

--- src/api/config.js
+++ src/api/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyCX-vM3JkbLC5dSBBeDQAgmmGEKU_269gY',
-	authDomain: 'tcl-51-smart-shopping-list.firebaseapp.com',
-	projectId: 'tcl-51-smart-shopping-list',
-	storageBucket: 'tcl-51-smart-shopping-list.appspot.com',
-	messagingSenderId: '970974663458',
-	appId: '1:970974663458:web:c60c278055491604890b7b',
+	apiKey: 'AIzaSyBi--Og0SaKCugMrZ6xtH23RRDmJI4P1KM',
+	authDomain: 'smart-shopping-list-c7108.firebaseapp.com',
+	projectId: 'smart-shopping-list-c7108',
+	storageBucket: 'smart-shopping-list-c7108.appspot.com',
+	messagingSenderId: '1040087405685',
+	appId: '1:1040087405685:web:7136928570d9e64fde943c',
 };
 
 // Initialize Firebase
